a2df585fa1324a9103cb56f3f35ea6846ce0de8e
marcosd122 <marcos.adv.guimaraes@gmail.com>
2022-09-18 17:36:56 -0400

    Atualização

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -15,6 +16,7 @@ const Header = () => {
         <h5 className="text-light">Rápido, fácil e acessível a todos</h5>
 
         <CTA />
+        <HeaderSocials />
 
         <div className="me">
           <img src={ME} alt="me" />
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import {BsFacebook} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+import {BsLinkedin} from 'react-icons/bs'
+
 
 const HeaderSocials = () => {
   return (
-    <div>
-        
+    <div className='header__socials'>
+        <a href="https://www.facebook.com/ifrondonia/" target="_blank" rel="noreferrer"><BsFacebook/></a>
+        <a href="https://github.com/IFRO" target="_blank" rel="noreferrer"><FaGithub/></a>
+        <a href="https://br.linkedin.com/school/institutofederalderondonia" target="_blank" rel="noreferrer"><BsLinkedin/></a>
     </div>
   )
 }
