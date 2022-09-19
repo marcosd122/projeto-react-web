b7a313418307b9b623774a76a9381d21a039a1b7
marcosd122 <marcos.adv.guimaraes@gmail.com>
2022-09-18 20:55:22 -0400

    Atualização

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import SOBRE from '../../assets/sobre1.png'
+import SOBRE from '../../assets/sobre.png'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
