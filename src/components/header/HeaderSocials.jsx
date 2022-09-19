82f8fcea189115f37c59449ad7f41e5f506a835b
marcosd122 <marcos.adv.guimaraes@gmail.com>
2022-09-18 23:48:18 -0400

    Atualização

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React from 'react'
 import {BsFacebook} from 'react-icons/bs'
 import {FaGithub} from 'react-icons/fa'
@@ -7,9 +8,9 @@ import {BsLinkedin} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://www.facebook.com/ifrondonia/" target="_blank" rel="noreferrer"><BsFacebook/></a>
-        <a href="https://github.com/IFRO" target="_blank" rel="noreferrer"><FaGithub/></a>
-        <a href="https://br.linkedin.com/school/institutofederalderondonia" target="_blank" rel="noreferrer"><BsLinkedin/></a>
+        <a href="https://www.facebook.com/ifrondonia/" target="_blank"><BsFacebook/></a>
+        <a href="https://github.com/IFRO" target="_blank"><FaGithub/></a>
+        <a href="https://br.linkedin.com/school/institutofederalderondonia" target="_blank"><BsLinkedin/></a>
     </div>
   )
 }
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,23 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import './nav.css'
+import  {AiOutlineHome} from 'react-icons/ai'
+import  {AiOutlineUser} from 'react-icons/ai'
+import  {BiBook} from 'react-icons/bi'
+import  {RiServiceLine} from 'react-icons/ri'
+import  {BiMessageSquareDetail} from 'react-icons/bi'
+
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>      
+      <a href="#"
+      className='active'><AiOutlineHome/></a> 
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><BiBook/></a>
+      <a href="#services"><RiServiceLine/></a>
+      <a href="#contact"><BiMessageSquareDetail/></a>
+    </nav>
   )
 }
 
