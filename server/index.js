9309015880e6f09f23f96f294d02ef6ba2ed359e
marcosd122 <marcos.adv.guimaraes@gmail.com>
2022-10-16 12:13:35 -0400

    Atualização

--- server/index.js
+++ server/index.js
@@ -1,2 +1,19 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql')
+
+const db = mysql.createPool({
+host: 'localhost',
+user: 'root',
+password: 'Guimaraes2',
+database: 'db'
+});
+
+
+app.get('/', (req, res) => {})
+
+//Parâmetros de função
+
+app.listen(3001, () => {
+    console.log('Rodando na porta 3001')
+});
